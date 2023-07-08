a2fc35c1eea2b935f679ed02e2060f7cfb85c32d
Jeki07 <esenaliev@gmail.com>
2023-07-08 13:50:52 +0600

    settings static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('reg/', include('registration_user.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
